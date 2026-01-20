df5b7b09eb0980343e56f809c21869649f923aa0
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-01-20 17:53:51 +0000

    adicionando componente de colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador';
 import './Time.css'
 
 const Time = (props) => {
@@ -5,8 +6,12 @@ const Time = (props) => {
         backgroundColor: props.corSecundaria 
     }
     return (
+        props.colaboradores.length > 0 && 
         <section className='time' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+            </div>
         </section>
     )
 }
